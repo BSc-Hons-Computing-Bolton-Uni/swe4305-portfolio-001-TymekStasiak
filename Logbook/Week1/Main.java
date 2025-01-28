ff0c9d23c24e33b89e27638845be43c6e644748f
Mark_Thwaite <100203324@Student.burnley.ac.uk>
2025-01-28 13:59:57 +0000

    test

--- Logbook/Week1/Main.java
+++
@@ -1,7 +0,0 @@
-// Hello World Program
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ Logbook/Week1/main.java
@@ -0,0 +1,13 @@
+// Hello World Program
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args)
+    {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, what's your name?");
+        name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
