1f1a776927bc9ad8d6fa779fe8ebac6cada2e78a
TymekStasiak <100195304@student.burnley.ac.uk>
2025-03-03 10:34:56 +0000

    update

--- Logbook/Week3/MainTask1.java
+++ Logbook/Week3/MainTask1.java
@@ -19,7 +19,30 @@ class Student {
     }
 
     public void print() {
-        System.out.println("Student ID: " + studentID + ", Name " + name);
+        System.out.println("Student ID: " + studentID + ", Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student(100195304, "Tymoteusz Stasiak");
+        student1.print();
     }
 }
 
+class Course {
+
+    private String courseCode;
+    private String courseName;
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
