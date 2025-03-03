2012308a33471814dac6986d1455208c278e9326
TymekStasiak <100195304@student.burnley.ac.uk>
2025-03-03 12:48:58 +0000

    update

--- Logbook/Week2/MainTask8.java
+++ Logbook/Week2/MainTask8.java
@@ -2,7 +2,7 @@ package Logbook.Week2;
 
 class Loop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 20; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
--- Logbook/Week3/MainTask1.java
+++ Logbook/Week3/MainTask1.java
@@ -4,10 +4,12 @@ class Student {
 
     public int studentID;
     public String name;
+    private Course course;
 
-    public Student(int studentID, String name) {
+    public Student(int studentID, String name, Course course) {
         this.studentID = studentID;
         this.name = name;
+        this.course = course;
     }
 
     public int getStudentID() {
@@ -18,13 +20,21 @@ class Student {
         return name;
     }
 
-    public void print() {
-        System.out.println("Student ID: " + studentID + ", Name: " + name);
+    public Course getCourse() {
+        return course;
     }
 
-    public static void main(String[] args) {
-        Student student1 = new Student(100195304, "Tymoteusz Stasiak");
-        student1.print();
+    public void enrol(Course newCourse) {
+        this.course = newCourse;
+    }
+
+    public void print() {
+        System.out.println("Student ID: " + studentID + ", Name: " + name);
+        if (course != null) {
+            course.print();
+        } else {
+            System.out.println("No course enrolled.");
+        }
     }
 }
 
@@ -44,5 +54,24 @@ class Course {
 
     public String getCourseName() {
         return courseName;
+    }
+
+    public void print() {
+        System.out.println("Course Code: " + courseCode + ", Name: " + courseName);
+    }
+}
+
+class MainTask {
+    public static void main(String[] args) {
+        Course course1 = new Course("001", "English");
+        Course course2 = new Course("002", "Mathematics");
+
+        Student student1 = new Student(100195304, "Tymoteusz Stasiak", course1);
+        student1.print();
+
+        System.out.println("\nEnrolling student in a new course.\n");
+
+        student1.enrol(course2);
+        student1.print();
     }
 }
