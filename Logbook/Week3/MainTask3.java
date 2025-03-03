8ce97e7f22e457b417d05b99066470fb9155a646
TymekStasiak <100195304@student.burnley.ac.uk>
2025-03-03 09:47:31 +0000

    update

--- Logbook/Week3/MainTask3.java
+++ Logbook/Week3/MainTask3.java
@@ -1,4 +1,24 @@
 package Logbook.Week3;
 
-class MainTask3 {
+class Student {
+
+    public int studentID;
+    public String name;
+
+    public Student(int studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void print() {
+        System.out.println("Student ID: " + studentID + ", Name " + name);
+    }
 }
+
